675c13c000b5efc892c92c60b4de66b2d33ddadd
Edwin Blanco <edwinblanco316@gmail.com>
2021-11-04 13:36:31 -0500

    Se crearon las clases Usuario y Mascota y se crearon 4 pantallas iniciales v2

---
+++ app/src/main/java/com/example/adoptpetapp/view/activity/PublicarMascotaActivity.java
@@ -0,0 +1,16 @@
+package com.example.adoptpetapp.view.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.adoptpetapp.R;
+
+public class PublicarMascotaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_publicar_mascota);
+    }
+}
