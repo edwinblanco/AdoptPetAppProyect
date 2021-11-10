9f7d06dac5404d34006c017c901fb5ff9eebe49d
Rozco01 <jeffer.rozo.123@gmail.com>
2021-11-10 16:59:33 -0500

    Implementacion de inicio de sesion y registro

--- app/src/main/java/com/example/adoptpetapp/view/activity/MainActivity.java
+++ app/src/main/java/com/example/adoptpetapp/view/activity/MainActivity.java
@@ -1,16 +1,79 @@
 package com.example.adoptpetapp.view.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.adoptpetapp.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnRegistro;
+    private FirebaseAuth mAuth;
+    private EditText correo;
+    private EditText contrasena;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mAuth = FirebaseAuth.getInstance();
+
+        btnRegistro = findViewById(R.id.btRegistrar);
+        btnRegistro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, RegistroUsuarioActivity.class);
+                startActivity(i);
+            }
+        });
+
+        correo = findViewById(R.id.Usuario);
+        contrasena = findViewById(R.id.Contrasena);
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+    }
+
+    public void iniciarSesion(View view){
+        mAuth.signInWithEmailAndPassword(correo.getText().toString().trim(), contrasena.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(getApplicationContext(), "Datos correctos",
+                                    Toast.LENGTH_SHORT).show();
+                            Intent i = new Intent(getApplicationContext(), PublicarMascotaActivity.class);
+                            startActivity(i);
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(getApplicationContext(), "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
 }
--- app/src/main/java/com/example/adoptpetapp/view/activity/RegistroUsuarioActivity.java
+++ app/src/main/java/com/example/adoptpetapp/view/activity/RegistroUsuarioActivity.java
@@ -1,16 +1,81 @@
 package com.example.adoptpetapp.view.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.adoptpetapp.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegistroUsuarioActivity extends AppCompatActivity {
 
+
+    private FirebaseAuth mAuth;
+    private Button btnInicio;
+
+    private EditText correo;
+    private EditText contrasena;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_usuario);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        correo = findViewById(R.id.UsuarioRegistro);
+        contrasena = findViewById(R.id.ContraseñaRegistro);
+        btnInicio= findViewById(R.id.btVolverSesion);
+
+        btnInicio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(RegistroUsuarioActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+    }
+
+    public void registrarUsuario(View view){
+        mAuth.createUserWithEmailAndPassword(correo.getText().toString().trim(), contrasena.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+
+                            Intent i = new Intent(getApplicationContext(), MainActivity.class);
+                            startActivity(i);
+                            Toast.makeText(getApplicationContext(), "Creado Exitosamente",Toast.LENGTH_SHORT).show();
+
+                            //updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(getApplicationContext(), "Authentication failed.",Toast.LENGTH_SHORT).show();
+                            //updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
 }
